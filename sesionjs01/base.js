a6144cade427a930c1bea6e60bdf1f292ac0375f
Sandra Tapia <sandra.tagon@outlook.com>
2022-06-29 22:27:03 -0500

    Sesiónjs-01

---
+++ sesionjs01/base.js
@@ -0,0 +1,41 @@
+console.log('Hola')
+
+//! comentarios en JS
+
+//esto es un comentario//
+
+/*esto es un
+comentario 
+multilínea*/
+
+// !datos en JS
+
+dato : fragmento de una información.
+ //identificador
+  //reglas o buenas prácticas
+   //no inician con número
+
+  
+
+   //no acepta palabras reservadas(palabras del lenguaje de programación)
+    
+
+    //consideración ; es sensible a las mayúsculas y minúsculas
+
+
+    //buena práctica: uso de camelCase ,PascalCase ->clases- CONSTANTE
+
+    // buena práctica:naming descriptivo
+    //reglas: no deben contener caracteres especiales
+
+    //! variables y constantes
+      
+    //variable = mutabilidad (VALOR QUE CAMBIA CONSTANTEMENTE)
+     //constante = No cambia su valor
+
+     //variables : palabras reservadas = var, let 
+     let edadKoder = 25;
+     var alturaKoder= 165;
+
+     console.log(edadKoder);
+     console.log(alturaKoder);
